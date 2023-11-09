b80d4490e9672f0093117010882f142a46f6458e
rcshpark <rcshpark@gmail.com>
2023-11-09 18:06:19 +0900

    UI연결

--- WeatherApp/WeatherApp/ViewController.swift
+++ WeatherApp/WeatherApp/ViewController.swift
@@ -4,16 +4,24 @@
 //
 //  Created by 박상훈 on 2023/11/09.
 //
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var cityTextField: UITextField!
+    @IBAction func loadWeatherBtn(_ sender: Any) {
+        if let cityName = self.cityTextField.text {
+            
+        }
+    }
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var weatherLabel: UILabel!
+    @IBOutlet weak var temLabel: UILabel!
+    @IBOutlet weak var minLabel: UILabel!
+    @IBOutlet weak var maxLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
 
