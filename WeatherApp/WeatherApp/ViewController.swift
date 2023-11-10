a2ca97cb075d7f10f5abd399b426a82cdba7afef
rcshpark <rcshpark@gmail.com>
2023-11-10 23:20:53 +0900

    날씨 데이터 불러오기 버튼 이벤트 적용

--- WeatherApp/WeatherApp/ViewController.swift
+++ WeatherApp/WeatherApp/ViewController.swift
@@ -10,7 +10,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var cityTextField: UITextField!
     @IBAction func loadWeatherBtn(_ sender: Any) {
         if let cityName = self.cityTextField.text {
-            
+            self.getCurrentWeather(cityName: cityName)
+            self.view.endEditing(true)
         }
     }
     @IBOutlet weak var cityLabel: UILabel!
@@ -22,6 +23,16 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    func getCurrentWeather(cityName: String){
+        guard let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?q=\(cityName)&appid=1b0c39a6a1e60feafa1a3674910a65af") else { return }
+        let session = URLSession(configuration: .default)
+        session.dataTask(with: url){ data, response, error in
+            guard let data = data, error == nil else { return }
+            let decoder = JSONDecoder()
+            let weatherInformation = try? decoder.decode(WeatherInformation.self, from: data)
+            debugPrint(weatherInformation)
+        }.resume()
     }
 }
 
